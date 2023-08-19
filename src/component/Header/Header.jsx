2581fdb538a4431badb24bc549740e2ad185a569
Arif Hossan <arif.hossan.eu@gmail.com>
2023-08-20 01:20:33 +0600

    header section added

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,18 @@
+import logo from "../../images/Logo.svg";
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav className="header">
+            <img src={logo} alt="" />
+            <div>
+                <a href="/Shop">Shop</a>
+                <a href="/Order">Order</a>
+                <a href="/Inventor">Inventor</a>
+                <a href="/Login">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
