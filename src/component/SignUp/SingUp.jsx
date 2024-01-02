80b7dfdb55baf778f7285f96f90d6ea725bfc7e5
Arif Hossan <arif.hossan.eu@gmail.com>
2024-01-03 05:25:26 +0600

    styling login and signup form

--- src/component/SignUp/SingUp.jsx
+++ src/component/SignUp/SingUp.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const SingUp = () => {
+    const [error,setError]= useState('');
+    const handleSignUp = (event) => {
+        // stop reloading
+        event.preventDefault();
+        // 
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirmPassword = form.confirmPassword.value;
+        // console.log(email,password,confirmPassword);
+        // password validation
+        if(password != confirmPassword){
+            setError("Your password didn't match");
+            return;
+        }
+        else if(password.length < 6){
+            setError('Password must be 6 characters or longer');
+            return;
+        }
+    }
     return (
         <div className='form-container'>
         <h2 className="form-title">Sign Up</h2>
-        <form action="">
+        <form onSubmit={handleSignUp} action="">
             <div className="form-control">
                 <label htmlFor="email">Email</label>
                 <input type="email" name="email"  placeholder='Enter your email' required/>
@@ -15,10 +36,12 @@ const SingUp = () => {
             </div>
             <div className="form-control">
                 <label htmlFor="password">Confirm Password</label>
-                <input type="password" name="confirm_password"  placeholder='Retype your password' required/>
+                <input type="password" name="confirmPassword"  placeholder='Retype your password' required/>
             </div>
             <input className='btn-submit' type="submit" value="Sign Up" />
         </form>
+        <p><small>Already have an account ? <Link to='/login'>Login</Link></small></p>
+        <p><small className='text-error'>{error}</small></p>
     </div>
     );
 };
