80d4340f6664ef1dc4c78c374ef7eafdfbdd13aa
Arif Hossan <arif.hossan.eu@gmail.com>
2024-01-03 00:48:20 +0600

    create signup and login form

--- src/component/SignUp/SingUp.jsx
+++ src/component/SignUp/SingUp.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 
 const SingUp = () => {
     return (
-        <div>
-            <h2>This is from signup</h2>
-        </div>
+        <div className='form-container'>
+        <h2 className="form-title">Sign Up</h2>
+        <form action="">
+            <div className="form-control">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email"  placeholder='Enter your email' required/>
+            </div>
+            <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password"  placeholder='Enter your password' required/>
+            </div>
+            <div className="form-control">
+                <label htmlFor="password">Confirm Password</label>
+                <input type="password" name="confirm_password"  placeholder='Retype your password' required/>
+            </div>
+            <input className='btn-submit' type="submit" value="Sign Up" />
+        </form>
+    </div>
     );
 };
 
